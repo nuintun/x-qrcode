44f0d9ec34b32a6a039723e3c5a9c9e1c7cdccdc
Nuintun <nuintun@qq.com>
2014-10-16 10:36:02 +0800

    bug fixed

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,19 @@
 /**
- * 监听标签更新事件并显示扩展按钮
+ * 获取初始化标签并显示pageAction按钮
+ */
+chrome.tabs.getSelected(function (tab){
+    chrome.pageAction.show(tab.id);
+});
+
+/**
+ * 监听标签的选择变更事件并显示pageAction按钮
+ */
+chrome.tabs.onSelectionChanged.addListener(function (uuid){
+    chrome.pageAction.show(uuid);
+});
+
+/**
+ * 监听标签的状态更新事件并显示pageAction按钮
  */
 chrome.tabs.onUpdated.addListener(function (uuid){
     chrome.pageAction.show(uuid);
