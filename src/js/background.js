d6833bd599f3a1808a83c7185a332dbff5daf51a
Nuintun <nuintun@qq.com>
2015-05-21 15:49:09 +0800

    adjust code style

--- src/js/background.js
+++ src/js/background.js
@@ -1,217 +1,217 @@
 // 同步锁
 var Sync = {
-    SYNCLOCK: {},
-    lock: function (uuid, callback){
-        var that = this;
+  SYNCLOCK: {},
+  lock: function (uuid, callback){
+    var that = this;
 
-        if (!that.SYNCLOCK[uuid] && typeof callback === 'function') {
-            that.SYNCLOCK[uuid] = true;
+    if (!that.SYNCLOCK[uuid] && typeof callback === 'function') {
+      that.SYNCLOCK[uuid] = true;
 
-            callback(function (){
-                delete that.SYNCLOCK[uuid];
-            });
-        }
+      callback(function (){
+        delete that.SYNCLOCK[uuid];
+      });
     }
+  }
 };
 
 /**
  * 获取初始化标签并显示pageAction按钮
  */
 chrome.tabs.getSelected(function (tab){
-    chrome.pageAction.show(tab.id);
+  chrome.pageAction.show(tab.id);
 });
 
 /**
  * 监听标签的选择变更事件并显示pageAction按钮
  */
 chrome.tabs.onSelectionChanged.addListener(function (uuid){
-    chrome.pageAction.show(uuid);
+  chrome.pageAction.show(uuid);
 });
 
 /**
  * 监听标签的状态更新事件并显示pageAction按钮
  */
 chrome.tabs.onUpdated.addListener(function (uuid){
-    chrome.pageAction.show(uuid);
+  chrome.pageAction.show(uuid);
 });
 
 /**
  * 监听标签请求绘制二维码并返回
  */
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse){
-    if (request.action === 'QREncodeLink') {
-        Sync.lock('PAGEACTION-' + request.tab.id, function (unlock){
-            QRCode.QREncode({
-                text: request.tab.url,
-                moduleSize: 3,
-                margin: 2,
-                logo: 'images/qrlogo.ico',
-                success: function (canvas){
-                    sendResponse({
-                        valid: true,
-                        srcUrl: canvas.toDataURL('image/png')
-                    });
+  if (request.action === 'QREncodeLink') {
+    Sync.lock('PAGEACTION-' + request.tab.id, function (unlock){
+      QRCode.QREncode({
+        text: request.tab.url,
+        moduleSize: 3,
+        margin: 2,
+        logo: 'images/qrlogo.ico',
+        success: function (canvas){
+          sendResponse({
+            valid: true,
+            srcUrl: canvas.toDataURL('image/png')
+          });
 
-                    // 解锁
-                    unlock();
-                },
-                error: function (e){
-                    var message = e.errorCode === 2
-                        ? '网址长度超过了二维码的最大存储上限！'
-                        : e.message;
+          // 解锁
+          unlock();
+        },
+        error: function (e){
+          var message = e.errorCode === 2
+            ? '网址长度超过了二维码的最大存储上限！'
+            : e.message;
 
-                    sendResponse({
-                        valid: false,
-                        message: message
-                    });
+          sendResponse({
+            valid: false,
+            message: message
+          });
 
-                    // 解锁
-                    unlock();
-                }
-            });
-        });
-    }
+          // 解锁
+          unlock();
+        }
+      });
+    });
+  }
 });
 
 /**
  * 添加右键解析二维码菜单
  */
 chrome.contextMenus.create({
-    id: 'QRDecode',
-    title: '解码所选图片',
-    contexts: ['image'],
-    onclick: function (data, tab){
-        Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
-            var image = new Image(),
-                canvas = document.createElement('canvas'),
-                ctx = canvas.getContext('2d');
+  id: 'QRDecode',
+  title: '解码所选图片',
+  contexts: ['image'],
+  onclick: function (data, tab){
+    Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
+      var image = new Image(),
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
 
-            image.onload = function (){
-                var text;
+      image.onload = function (){
+        var text;
 
-                canvas.width = image.width;
-                canvas.height = image.height;
-                ctx.drawImage(image, 0, 0, image.width, image.height);
-                image.onload = null;
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+        image.onload = null;
 
-                text = QRCode.QRDecode(canvas, function (){
-                    chrome.tabs.sendRequest(tab.id, {
-                        valid: false,
-                        message: '图片解码失败，请选择标准二维码图片进行解码！',
-                        menuItemId: data.menuItemId
-                    });
-                });
-
-                text && chrome.tabs.sendRequest(tab.id, {
-                    text: text,
-                    valid: true,
-                    menuItemId: data.menuItemId
-                });
-
-                // 解锁
-                unlock();
-            };
-
-            image.onerror = function (){
-                chrome.tabs.sendRequest(tab.id, {
-                    valid: false,
-                    message: '图片加载失败，无法获取图片数据！',
-                    menuItemId: data.menuItemId
-                });
-
-                // 解锁
-                unlock();
-
-                image.onerror = null;
-            };
-
-            image.src = data.srcUrl;
+        text = QRCode.QRDecode(canvas, function (){
+          chrome.tabs.sendRequest(tab.id, {
+            valid: false,
+            message: '图片解码失败，请选择标准二维码图片进行解码！',
+            menuItemId: data.menuItemId
+          });
         });
-    }
+
+        text && chrome.tabs.sendRequest(tab.id, {
+          text: text,
+          valid: true,
+          menuItemId: data.menuItemId
+        });
+
+        // 解锁
+        unlock();
+      };
+
+      image.onerror = function (){
+        chrome.tabs.sendRequest(tab.id, {
+          valid: false,
+          message: '图片加载失败，无法获取图片数据！',
+          menuItemId: data.menuItemId
+        });
+
+        // 解锁
+        unlock();
+
+        image.onerror = null;
+      };
+
+      image.src = data.srcUrl;
+    });
+  }
 });
 
 /**
  * 添加右键编码链接地址
  */
 chrome.contextMenus.create({
-    id: 'QREncodeLink',
-    title: '编码所选链接',
-    contexts: ['link'],
-    onclick: function (data, tab){
-        Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
-            QRCode.QREncode({
-                text: data.linkUrl,
-                moduleSize: 3,
-                margin: 2,
-                logo: 'images/qrlogo.ico',
-                success: function (canvas){
-                    chrome.tabs.sendRequest(tab.id, {
-                        valid: true,
-                        menuItemId: data.menuItemId,
-                        srcUrl: canvas.toDataURL('image/png')
-                    });
+  id: 'QREncodeLink',
+  title: '编码所选链接',
+  contexts: ['link'],
+  onclick: function (data, tab){
+    Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
+      QRCode.QREncode({
+        text: data.linkUrl,
+        moduleSize: 3,
+        margin: 2,
+        logo: 'images/qrlogo.ico',
+        success: function (canvas){
+          chrome.tabs.sendRequest(tab.id, {
+            valid: true,
+            menuItemId: data.menuItemId,
+            srcUrl: canvas.toDataURL('image/png')
+          });
 
-                    // 解锁
-                    unlock();
-                },
-                error: function (e){
-                    var message = e.errorCode === 2
-                        ? '网址长度超过了二维码的最大存储上限！'
-                        : '编码错误，请刷新重试！';
+          // 解锁
+          unlock();
+        },
+        error: function (e){
+          var message = e.errorCode === 2
+            ? '网址长度超过了二维码的最大存储上限！'
+            : '编码错误，请刷新重试！';
 
-                    chrome.tabs.sendRequest(tab.id, {
-                        valid: false,
-                        message: message,
-                        menuItemId: data.menuItemId
-                    });
+          chrome.tabs.sendRequest(tab.id, {
+            valid: false,
+            message: message,
+            menuItemId: data.menuItemId
+          });
 
-                    // 解锁
-                    unlock();
-                }
-            });
-        });
-    }
+          // 解锁
+          unlock();
+        }
+      });
+    });
+  }
 });
 
 /**
  * 添加右键编码选中文本
  */
 chrome.contextMenus.create({
-    id: 'QREncodeSelection',
-    title: '编码所选文本',
-    contexts: ['selection'],
-    onclick: function (data, tab){
-        Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
-            QRCode.QREncode({
-                text: data.selectionText,
-                moduleSize: 3,
-                margin: 2,
-                logo: 'images/qrlogo.ico',
-                success: function (canvas){
-                    chrome.tabs.sendRequest(tab.id, {
-                        valid: true,
-                        menuItemId: data.menuItemId,
-                        srcUrl: canvas.toDataURL('image/png')
-                    });
+  id: 'QREncodeSelection',
+  title: '编码所选文本',
+  contexts: ['selection'],
+  onclick: function (data, tab){
+    Sync.lock('CONTEXTMENUS-' + tab.id, function (unlock){
+      QRCode.QREncode({
+        text: data.selectionText,
+        moduleSize: 3,
+        margin: 2,
+        logo: 'images/qrlogo.ico',
+        success: function (canvas){
+          chrome.tabs.sendRequest(tab.id, {
+            valid: true,
+            menuItemId: data.menuItemId,
+            srcUrl: canvas.toDataURL('image/png')
+          });
 
-                    // 解锁
-                    unlock();
-                },
-                error: function (e){
-                    var message = e.errorCode === 2
-                        ? '文本长度超过了二维码的最大存储上限！'
-                        : '编码错误，请刷新重试！';
+          // 解锁
+          unlock();
+        },
+        error: function (e){
+          var message = e.errorCode === 2
+            ? '文本长度超过了二维码的最大存储上限！'
+            : '编码错误，请刷新重试！';
 
-                    chrome.tabs.sendRequest(tab.id, {
-                        valid: false,
-                        message: message,
-                        menuItemId: data.menuItemId
-                    });
+          chrome.tabs.sendRequest(tab.id, {
+            valid: false,
+            message: message,
+            menuItemId: data.menuItemId
+          });
 
-                    // 解锁
-                    unlock();
-                }
-            });
-        });
-    }
+          // 解锁
+          unlock();
+        }
+      });
+    });
+  }
 });
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,34 +1,34 @@
 var QRBox = $('#qr-box'), // 二维码显示区域
-    ErrorTips = $('#error-tips'); // 错误信息显示区域
+  ErrorTips = $('#error-tips'); // 错误信息显示区域
 
 /**
  * 显示错误消息
  * @param message
  */
 function showError(message){
-    QRBox.hide();
-    ErrorTips.html(message).show();
+  QRBox.hide();
+  ErrorTips.html(message).show();
 }
 
 /**
  * 向背景页请求获取二维码
  */
 chrome.tabs.getSelected(function (tab){
-    chrome.extension.sendRequest({
-        tab: tab,
-        action: 'QREncodeLink'
-    }, function (response){
-        if (response.valid) {
-            QRBox.html('<img src="' + response.srcUrl + '" alt="QRCode"/>')
-        } else {
-            showError(response.message)
-        }
-    });
+  chrome.extension.sendRequest({
+    tab: tab,
+    action: 'QREncodeLink'
+  }, function (response){
+    if (response.valid) {
+      QRBox.html('<img src="' + response.srcUrl + '" alt="QRCode"/>')
+    } else {
+      showError(response.message)
+    }
+  });
 });
 
 /**
  * 禁用弹出页右键
  */
 $(document).on('contextmenu', function (e){
-    e.preventDefault();
+  e.preventDefault();
 });
